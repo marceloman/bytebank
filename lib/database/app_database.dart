7d0768af4dcefeca15a62b308fef996ae459d5f3
Marcelo Alvares <marcelo_alvares23@outlook.com>
2019-12-29 23:50:32 -0300

    second commit

---
+++ lib/database/app_database.dart
@@ -0,0 +1,47 @@
+import 'package:bytebank/models/contact.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+Future<Database> createDatabase() {
+  return getDatabasesPath().then((dbPath) {
+    final String path = join(dbPath, 'bytebank.db');
+    return openDatabase(path, onCreate: (db, version) {
+      db.execute('CREATE TABLE contacts ('
+          'id INTEGER PRIMARY KEY , '
+          'name TEXT, '
+          'account_number INTEGER)');
+    }, version: 1);
+  });
+}
+
+Future<int> save(Contact contact) {
+  final Map<String, dynamic> contactMap = Map();
+  return createDatabase().then((db) {
+    contactMap['name'] = contact.name;
+    contactMap['account_number'] = contact.accountNumber;
+    debugPrint(
+        'numero que chegou pra salvar: ' + contact.accountNumber.toString());
+    debugPrint('numero que esta sendo salvo: ' +
+        contactMap['account_number'].toString());
+    return db.insert('contacts', contactMap);
+  });
+}
+
+Future<List<Contact>> findAll() {
+  return createDatabase().then((db) {
+    return db.query('contacts').then((maps) {
+      final List<Contact> contacts = List();
+      for (Map<String, dynamic> map in maps) {
+        final Contact contact = Contact(
+          map['id'],
+          map['name'],
+          map['account_number'],
+        );
+        debugPrint('numero da conta que foi pesquisado: ' +map['account_number'].toString());
+        contacts.add(contact);
+      }
+      return contacts;
+    });
+  });
+}
---
+++ lib/screens/contact_form.dart
@@ -0,0 +1,62 @@
+import 'package:bytebank/models/contact.dart';
+import 'package:flutter/material.dart';
+
+class ContactForm extends StatefulWidget {
+  @override
+  _ContactFormState createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController _nameController = TextEditingController();
+
+  final TextEditingController _accountNumber = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Contact'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: 'Full name',
+                ),
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+            TextField(
+              controller: _accountNumber,
+              decoration: InputDecoration(
+                labelText: 'Account number',
+              ),
+              style: TextStyle(fontSize: 24.0),
+              keyboardType: TextInputType.number,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: RaisedButton(
+                  child: Text('Create'),
+                  onPressed: () {
+                    final String name = _nameController.text;
+                    final int accountNumber = int.tryParse(_accountNumber.text);
+                    final Contact newContact = Contact(0,name, accountNumber);
+                    Navigator.pop(context, newContact);
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
