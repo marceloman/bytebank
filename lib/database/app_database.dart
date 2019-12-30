9f59a5ae6ede5a2b362cdac26becb1648569b3a9
Marcelo Alvares <inf_malvares@uolinc.com>
2019-12-30 18:54:48 -0300

    BYTEBANK | começando async

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -3,7 +3,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> createDatabase() {
+//Usando async
+Future<Database> getDatabase() async {
+  final String path = join(await getDatabasesPath(), 'bytebank.db');
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute('CREATE TABLE contacts ('
+          'id INTEGER PRIMARY KEY , '
+          'name TEXT, '
+          'account_number INTEGER)');
+    },
+    version: 1, /*onDowngrade: onDatabaseDowngradeDelete*/
+  );
+}
+
+/*
+getDatabase usando then 
+Future<Database> getDatabase() {
   return getDatabasesPath().then((dbPath) {
     final String path = join(dbPath, 'bytebank.db');
     return openDatabase(path, onCreate: (db, version) {
@@ -13,11 +30,24 @@ Future<Database> createDatabase() {
           'account_number INTEGER)');
     }, version: 1, /*onDowngrade: onDatabaseDowngradeDelete*/);
   });
+}*/
+
+
+//usando async
+Future<int> save(Contact contact) async{
+  final Database db = await getDatabase();
+  final Map<String, dynamic> contactMap = Map();
+    contactMap['name'] = contact.name;
+    contactMap['account_number'] = contact.accountNumber;
+    return db.insert('contacts', contactMap);
+  
 }
 
+/*
+Save usando then
 Future<int> save(Contact contact) {
   final Map<String, dynamic> contactMap = Map();
-  return createDatabase().then((db) {
+  return getDatabase().then((db) {
     contactMap['name'] = contact.name;
     contactMap['account_number'] = contact.accountNumber;
     debugPrint(
@@ -26,10 +56,10 @@ Future<int> save(Contact contact) {
         contactMap['account_number'].toString());
     return db.insert('contacts', contactMap);
   });
-}
+}*/
 
 Future<List<Contact>> findAll() {
-  return createDatabase().then((db) {
+  return getDatabase().then((db) {
     return db.query('contacts').then((maps) {
       final List<Contact> contacts = List();
       for (Map<String, dynamic> map in maps) {
